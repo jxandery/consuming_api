1fbc3d2c131082ba10151ff88aad9e7ee3922536
Jack Yeh <jack.yeh@gmail.com>
2015-06-02 09:35:39 -0600

    create service

--- obeset/app/services/schoolist_service.rb
+++ obeset/app/services/schoolist_service.rb
@@ -1,3 +1,12 @@
 class SchoolistService
+  attr_reader :connection
+
+  def initialize
+    @connection = Hurley::Client.new('http://localhost:3000/v1')
+  end
+
+  def schools
+    connection.get('schools')
+  end
 
 end
